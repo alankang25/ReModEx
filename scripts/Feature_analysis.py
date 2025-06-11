322f940d0ed5a98e22a6839a38d06fa49a61db65
alankang25 <alan.kang@emory.edu>
2025-06-11 18:03:47 +0000

    added plotting functionality and updated regression calculation methodology

--- scripts/Feature_analysis.py
+++ scripts/Feature_analysis.py
@@ -1,5 +1,6 @@
 #TODO: Clean up interim files and add comments
 #TODO: Add feature where you can exclude certain features (i.e. remove rows with certain features as 1)
+#TODO: Add comments that specify the equations used for feature importance calculations in sklearn
 
 #importing libraries
 import pandas as pd
@@ -11,6 +12,15 @@ import argparse
 def parse_args():
     p = argparse.ArgumentParser(
         description="Take ENCODE tsv and download BED files with highest FRiP scores"
+    )
+    
+    p.add_argument(
+        "-x", "--exclude_features",
+        type=str,
+        nargs='*',
+        default=[],
+        help="List of features to exclude from the feature matrix (e.g., ['feature1', 'feature2'])."
+        " Features should be specified as the name of the bed file without the .bed extension."
     )
 
     p.add_argument(
@@ -169,8 +179,11 @@ def calculate_log2_fold_change(control_bigwig, treatment_bigwig, merged_peaks=".
 #-------------------block for machine learning-------------------
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, roc_curve, auc, precision_recall_curve
 from sklearn.linear_model import Ridge
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
 
 def random_forest(feature_matrix, target_column='target'):
     """
@@ -220,6 +233,29 @@ def random_forest(feature_matrix, target_column='target'):
     # Print confusion matrix
     print("Confusion Matrix:")
     print(confusion_matrix(y_test, y_pred))
+
+    #---------------block for plotting performance metrics-------------------
+    # Plot ROC and Precision-Recall curves on the same figuer side by side
+    fpr, tpr, _ = roc_curve(y_test, rf_classifier.predict_proba(X_test)[:, 1])
+    roc_auc = auc(fpr, tpr)
+    precision, recall, _ = precision_recall_curve(y_test, rf_classifier.predict_proba(X_test)[:, 1])
+    pr_auc = auc(recall, precision)
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+    ax1.plot(fpr, tpr, color='orange', label=f'ROC curve (area = {roc_auc:.2f})')
+    ax1.plot([0, 1], [0, 1], color='grey', linestyle='--', alpha=0.5)
+    ax1.set_xlabel('False Positive Rate')
+    ax1.set_ylabel('True Positive Rate')
+    ax1.set_title('Receiver Operating Characteristic (ROC) Curve')
+    ax1.legend(loc='lower right')
+    ax2.plot(recall, precision, color='green', label=f'Precision-Recall curve (area = {pr_auc:.2f})')
+    ax2.plot([0, 1], [0.5, 0.5], color='grey', linestyle='--', alpha=0.5)
+    ax2.set_xlabel('Recall')
+    ax2.set_ylabel('Precision')
+    ax2.set_title('Precision-Recall Curve')
+    ax2.legend(loc='lower left')
+    plt.tight_layout()
+    plt.savefig("../data/output/roc_pr_curves.pdf", format='pdf')
+    #--------------------------end of block--------------------------------
     
     return rf_classifier
 
@@ -242,6 +278,18 @@ def random_forest_feature_importance(rf_classifier):
     print("Feature Importances:")
     print(feature_importance_df)
 
+    #---------------block for plotting MDI feature importances-------------------
+    plt.figure(figsize=(6, 8))
+    #plot top 30 features
+    feature_importance_plot = feature_importance_df.head(30)
+    sns.barplot(x='importance', y='feature', data=feature_importance_plot, color=sns.xkcd_rgb['dark peach'])
+    plt.title('MDI Feature Importances from RF Classifier')
+    plt.xlabel('Mean Decrease in Impurity (MDI)')
+    plt.ylabel('Feature')
+    plt.tight_layout()
+    plt.savefig("../data/output/feature_importances_rf.pdf", format='pdf')
+    #--------------------------end of block--------------------------------
+
     return feature_importance_df
 
 def ridge_regression(feature_matrix, target_column='target'):
@@ -251,63 +299,99 @@ def ridge_regression(feature_matrix, target_column='target'):
     feature_matrix: pandas DataFrame with features as columns and ATAC-seq peaks as rows with target as log2 fold change
     target_column: column name for the target variable (default is 'target')
     """
-    #remove nan values from the feature matrix
-    feature_matrix = feature_matrix.dropna()
-
-    # 1 if log2 fold change is less than -1, 0 if other
-    feature_matrix[target_column] = (feature_matrix[target_column] < -1).astype(int)
-
-    #balance the dataset by undersampling
-    class_counts = feature_matrix[target_column].value_counts()
-    min_class_count = class_counts.min()
-    balanced_data = pd.concat([
-        feature_matrix[feature_matrix[target_column] == cls].sample(min_class_count, random_state=3)
-        for cls in class_counts.index
-    ])
-    # Shuffle the balanced data
-    balanced_data = balanced_data.sample(frac=1, random_state=3).reset_index(drop=True)
     
-    # Create the feature matrix
-    feature_matrix = balanced_data
+    #remove inf and nan values from the feature matrix
+    feature_matrix = feature_matrix.replace([np.inf, -np.inf], np.nan).dropna()
 
     # Split the data into features and target
-    X = feature_matrix.drop(columns=[target_column])
+    X_df = feature_matrix.drop(columns=[target_column])
     y = feature_matrix[target_column]
 
-    # Initialize the Ridge regression model
-    ridge_model = Ridge(alpha=1.0, random_state=3)
-    
-    # Fit the model to the data
-    ridge_model.fit(X, y)
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X_df)
+
+    #subsample 70% of the data and run ridge regression 10 times with random seed --> save the average coefficients with standard deviation
+    indices = [feature_matrix.sample(frac=0.7, random_state=42 + i).index for i in range(10)]
+
+    coefficients = {col: [] for col in X_df.columns}
+
+    # Loop through each set of sampled indices
+    for index_list in indices:
+        # Convert index_list to positional indices for numpy array X
+        pos_indices = feature_matrix.index.get_indexer(index_list)
+        temp_X = X[pos_indices, :]
+        temp_y = y.iloc[pos_indices]
+        
+        # Fit ridge regression (adjust alpha as needed)
+        ridge_model = Ridge(alpha=1.0)
+        ridge_model.fit(temp_X, temp_y)
+        
+        # Store the coefficient for each feature
+        for j, col in enumerate(X_df.columns):
+            coefficients[col].append(ridge_model.coef_[j])
+
+    #calculate the mean and standard deviation of the coefficients for each feature
+    mean_coefficients = {col: np.mean(coefs) for col, coefs in coefficients.items()}
+    std_coefficients  = {col: np.std(coefs)  for col, coefs in coefficients.items()}
+
+    #sort the features by the mean coefficient in descending order
+    sorted_features = sorted(mean_coefficients.items(), key=lambda x: x[1], reverse=True)
 
     # Print coefficients
-    print("Ridge Regression Coefficients:")
-    print(ridge_model.coef_)
+    feature_importance_df = pd.DataFrame(sorted_features, columns=['feature', 'importance'])
+    feature_importance_df['std'] = feature_importance_df['feature'].map(std_coefficients)
 
-    return ridge_model
+    return feature_importance_df
 
-def calculate_feature_importance_ridge(ridge_model, feature_names):
+def plot_ridge(feature_importance_df):
     """
-    Calculate feature importance from the Ridge regression model.
-    Parameters:
-    ridge_model: trained Ridge regression model
-    feature_names: list of feature names
+    Calculate feature importance from Ridge regression coefficient df
     """
-    # Get coefficients
-    coefficients = ridge_model.coef_
-    # Create a DataFrame for feature importances
+    # Create a DataFrame for feature importance
     feature_importance_df = pd.DataFrame({
-        'feature': feature_names,
-        'importance': coefficients
+        'feature': feature_importance_df['feature'],
+        'importance': feature_importance_df['importance'],
+        'std': feature_importance_df['std']
     })
-    # Sort by importance
-    feature_importance_df = feature_importance_df.sort_values(by='importance', ascending=False)
-    
-    print("Feature Importances from Ridge Regression:")
-    print(feature_importance_df)
+
+    #---------------block for plotting Ridge feature importances-------------------
+    plot_df = feature_importance_df[['feature', 'importance']]
+    plt.figure(figsize=(6, 20))
+    # plot all values in a heatmap with a color gradient (zero as white in coolwarm)
+    sns.heatmap(
+        plot_df.set_index('feature'),
+        annot=True,
+        cmap='coolwarm',
+        center=0,
+        yticklabels=plot_df['feature']
+    )
+    plt.title('Feature Importances from Ridge Regression')
+    plt.ylabel('Feature')
+    plt.xlabel('Importance')
+    plt.tight_layout()
+    plt.savefig("../data/output/feature_importances_ridge.pdf", format='pdf')
+    #--------------------------end of block--------------------------------
 
     return feature_importance_df
 #--------------------------end of block--------------------------
+
+def remove_features(feature_matrix, exclude_features):
+    """
+    Remove rows with 1 in specified columns from the feature matrix.
+    Parameters:
+    feature_matrix: pandas DataFrame with features as columns and ATAC-seq peaks as rows
+    exclude_features: list of features to exclude from the feature matrix
+    """
+    #drop rows where any of the specified features are 1
+    for feature in exclude_features:
+        if feature in feature_matrix.columns:
+            feature_matrix = feature_matrix[feature_matrix[feature] != 1]
+        else:
+            print(f"Feature '{feature}' not found in the feature matrix. Skipping.")
+    #reset index
+    feature_matrix.reset_index(drop=True, inplace=True)
+    
+    return feature_matrix
 
 def main():
     # Define the list of bed files containing features
@@ -318,6 +402,8 @@ def main():
     args = parse_args()
     control_ATAC = args.control_ATAC
     treatment_ATAC = args.treatment_ATAC
+    exclude_features = args.exclude_features
+    print(f"Excluding features: {exclude_features}")
 
     #save bed and bigwig file paths as variables
     control_bed, treatment_bed, control_bigwig, treatment_bigwig = modify_args(control_ATAC, treatment_ATAC)
@@ -331,13 +417,17 @@ def main():
     # Make feature matrix
     feature_matrix = make_feature_matrix(bed_list, feature_df)
 
+    #TODO: exclude features are are specified through command line arguments --> drop rows with 1 in specified columns
+    if exclude_features:
+        feature_matrix = remove_features(feature_matrix, exclude_features)
+
     #random forest classifier
     rf_classifier = random_forest(feature_matrix)
     rf_feature_importance_df = random_forest_feature_importance(rf_classifier)
 
     #ridge regularized linear regression
-    ridge_model = ridge_regression(feature_matrix)
-    ridge_feature_importance_df = calculate_feature_importance_ridge(ridge_model, feature_matrix.columns[:-1])
+    ridge_df = ridge_regression(feature_matrix)
+    ridge_feature_importance_df = plot_ridge(ridge_df)
 
     #concat feature importances --> column names are 'feature', 'MDI_importance', 'Ridge_coef'
     feature_importance_df = pd.concat([
